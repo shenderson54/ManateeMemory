7c200a3e7ff4597af7c8df54d46f635b0bd9ce8f
kkholland <katherinebholland@gmail.com>
2021-03-09 00:18:00 -0500

    added more comments inside JS code

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 (function(){
 
 
-// Create a variable for the difficulty buttons so we can read which one is picked, make an empty level selection variable to save the selection
+// Read in level difficulty buttons and create variable to store selection
 const difficulty = document.querySelectorAll('.difficulty');
 let levelSelected = null;
 
-//Listening to each difficulty button 
+
 difficulty.forEach(level => level.addEventListener('click',(event) => {
-    //if new page and no level is selected yet- then set level selected to what was picked and add a background color to show selection
+    //if no level is selected yet- then set level and highlight selection orange
     if(!levelSelected){
         event.target.classList.add('orangeBackground');
         levelSelected = event.target.getAttribute('id')
     }
     
-    //if not new page and difficulty has been set before, undo background selection and reset the level to the new value.
+    //if level has been picked already, undo previous color and reset for new level
     else{
         document.getElementById(`${levelSelected}`).classList.remove('orangeBackground');
         event.target.classList.add('orangeBackground');
@@ -23,18 +23,28 @@ difficulty.forEach(level => level.addEventListener('click',(event) => {
 
 // Set # of cards switch statement based on levelSelected (hardcode for now)
 const numberOfCards = 6;
+
+
+
+
+//Need function to make a random array with doubles of icons, and mixed in a random order...
 const iconArray = ['diamond','diamond', 'bolt','bolt', 'anchor','anchor'];
+
+
+
+
+//Read in start button and section for cards to be created in
 const startGame = document.getElementById('start');
 const cardGrid = document.querySelector('.card-grid');
 
-
 startGame.addEventListener('click',() => {
-
+    //for every card needed, create a div for it with an icon inside
     for (let i = 0; i < numberOfCards; i++){
         let card = document.createElement('div');
         let icon = document.createElement('i');
 
         icon.setAttribute('class', `fa fa-${iconArray[i]}`);
+        card.setAttribute('id',iconArray[i]);
         card.classList.add('cardBack');
         
         card.append(icon);
