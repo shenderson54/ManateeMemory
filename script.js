38795ea5b7e47ddb31081a38eae7579c8eb99953
kkholland <katherinebholland@gmail.com>
2021-03-08 23:35:44 -0500

    js to read in difficulty level created, along with 1 css class for selection background (placeholder)

---
+++ script.js
@@ -0,0 +1,41 @@
+(function(){
+
+
+// Create a variable for the difficulty buttons so we can read which one is picked, make an empty level selection variable to save the selection
+const difficulty = document.querySelectorAll('.difficulty');
+let levelSelected = null;
+
+//Listening to each difficulty button 
+difficulty.forEach(level => level.addEventListener('click',(event) => {
+    //if new page and no level is selected yet- then set level selected to what was picked and add a background color to show selection
+    if(!levelSelected){
+        event.target.classList.add('orangeBackground');
+        levelSelected = event.target.getAttribute('id')
+    }
+    
+    //if not new page and difficuty has been set before, undo background selection and reset the level to the new value.
+    else{
+        document.getElementById(`${levelSelected}`).classList.remove('orangeBackground');
+        event.target.classList.add('orangeBackground');
+        levelSelected = event.target.getAttribute('id');
+    }
+}))
+
+// Set # of cards switch statement based on levelSelected (hardcode for now)
+const numberCards = 6;
+
+const startGame = document.getElementById('start');
+startGame.addEventListener('click',() => {
+
+    for (let i = 0; i < numberOfCards; i++){
+        let card = document.createElement('div');
+        // if (i === randomCell){
+        //     card.setAttribute('id','it');
+        // };
+        cell.classList.add('cell');
+        grid.append(cell);
+    }
+
+});
+
+})();
