b49e8c863087c91e48a7fbbe8a5e3f43d4f8c832
kkholland <katherinebholland@gmail.com>
2021-03-11 22:31:21 -0500

    congrats box styling, moves made/moves left logic, rearranged js variables and functions

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 (function(){
 
 
-// Default Home Page
-    const defaultPage = document.getElementsByTagName('body');
-    
-    
-
-
-
-// Read in level difficulty buttons and create variable to store selection
 const difficulty = document.querySelectorAll('.difficulty');
+const startGame = document.getElementById('start');
+const cardGrid = document.querySelector('.card-grid');
+const elementStart = document.querySelectorAll('.start-items');
+const gameControls = document.querySelectorAll('.game-controls');
+let timer = document.querySelector("#timer");
+let minutesLabel = document.getElementById("minutes");
+let secondsLabel = document.getElementById("seconds");
+
+const iconArray = ['gem','bolt','anchor','fish','crown','dragon','moon', 'heart','tree'];
+let randomIconArray =[];
+
 let levelSelected = null;
 let numberOfCards = null;
+let numberMovesMade = 0;
+let numberMatchesLeft = null;
+let totalSeconds = 0;
+let click1 = null;
+let click2 = null;
 
-difficulty.forEach(level => level.addEventListener('click',(event) => {
-    //if level has been picked already, undo previous color 
-    if(levelSelected){
-        document.getElementById(`${levelSelected}`).classList.remove('buttonSelectBackground');
-        
-    }
-    //Otherwise set background and read in difficulty level
-    event.target.classList.add('buttonSelectBackground');
-    levelSelected = event.target.id;
 
-}));
-
-// Set # of cards based on levelSelected 
+/**
+ * Set # of cards based on levelSelected 
+ */
 function setNumberOfCards(){
     switch(levelSelected){
         case 'easy':
@@ -42,11 +41,9 @@ function setNumberOfCards(){
 };
 
 
-//Array of icon names and placeholder for random order array
-const iconArray = ['gem','bolt','anchor','fish','crown','dragon','moon', 'heart','tree'];
-let randomIconArray =[];
-
-//Create random array of icon names based on numberOfCards
+/**
+ * Create random array of icon names based on numberOfCards
+ */
 function randomIcons(){
     if (randomIconArray.length > 0){
         randomIconArray.length=0;
@@ -65,9 +62,103 @@ function randomIcons(){
       randomIconArray[i] = randomIconArray[j];
       randomIconArray[j] = temp;
     }
-  }
+}
 
-//deletes any existing cards in cardGrid area
+
+/**
+ * Create a div for every card with the icon inside
+ */
+function createCards(){
+
+    for (let i = 0; i < numberOfCards; i++){
+        let card = document.createElement('div');
+        let cardInner = document.createElement('div');
+        let cardFront = document.createElement('div');
+        let cardBack = document.createElement('div');
+        let icon = document.createElement('i');
+
+        icon.classList.add('fas');
+        icon.classList.add(`fa-${randomIconArray[i]}`);
+        cardFront.id = randomIconArray[i];
+
+        cardInner.classList.add('flip-card-inner');
+        cardFront.classList.add('flip-card-front');
+        cardBack.classList.add('flip-card-back');
+        card.classList.add('flip-card');
+        
+        cardBack.append(icon);
+        cardInner.append(cardFront);
+        cardInner.append(cardBack);
+        card.append(cardInner);
+        cardGrid.append(card);
+    };
+}
+
+
+function cardsMismatch(card1,card2){
+    card1.parentNode.style.transform = "rotateY(0deg)";
+    card2.parentNode.style.transform = "rotateY(0deg)";
+    numberMovesMade += 1;
+    document.getElementById('moves-made').innerText = numberMovesMade;
+}
+
+
+function cardsMatch(card1,card2){
+    card1.parentNode.classList.add('visibility-hidden');
+    card2.parentNode.classList.add('visibility-hidden');
+
+    numberMatchesLeft -= 1;
+    document.getElementById('matches-left').innerText = numberMatchesLeft;
+
+    numberMovesMade += 1;
+    document.getElementById('moves-made').innerText = numberMovesMade;
+
+    //If number matches = 0 show congrats screen
+    if(numberMatchesLeft===0){
+        updateCongratsScreen();
+    }
+    
+}
+
+
+function updateCongratsScreen(){
+    cardGrid.classList.add('display-none');
+    document.querySelector('.popup').classList.remove('display-none');
+    document.getElementById("startMatches").innerText = numberOfCards/2;
+    document.getElementById("finalMove").innerText = numberMovesMade;
+    document.getElementById("totalMinutes").innerText = minutesLabel.innerText;
+    document.getElementById("totalSeconds").innerText = secondsLabel.innerText;
+}
+
+/**
+ * Function that starts the timer when the game starts
+ */
+const startTimer = () => {
+    const setTime = () => {
+    //I added this code in here (although not used yet) but maybe useful for when the game restarts.
+       if (numberMatchesLeft === 0) {
+       clearInterval(refreshTimer);
+      }
+      ++totalSeconds;
+      secondsLabel.innerHTML = pad(totalSeconds % 60);
+      minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+    };
+  
+    let refreshTimer = setInterval(setTime, 1000);
+    let pad = (val) => {
+      let valString = val + "";
+      if (valString.length < 2) {
+        return "0" + valString;
+      } else {
+        return valString;
+      }
+    };
+  };
+
+
+/**
+ * Deletes any existing cards in cardGrid area
+ */
 function clearCards() {
     let cards = document.querySelectorAll(".flip-card");
     if (cards.length > 0) {
@@ -78,61 +169,38 @@ function clearCards() {
     }
 }  
 
-//function that starts the timer when the game starts
-let timer = document.querySelector("#timer");
 
-let minutesLabel = document.getElementById("minutes");
-let secondsLabel = document.getElementById("seconds");
-let totalSeconds = 0;
-
-const startTimer = () => {
-  const setTime = () => {
-  //I added this code in here (although not used yet) but maybe useful for when the game restarts.
-  //    if (matches === numberOfCards) {
-  //    clearInterval(refreshTimer);
-  //   }
-    ++totalSeconds;
-    secondsLabel.innerHTML = pad(totalSeconds % 60);
-    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-  };
-
-  let refreshTimer = setInterval(setTime, 1000);
-  let pad = (val) => {
-    let valString = val + "";
-    if (valString.length < 2) {
-      return "0" + valString;
-    } else {
-      return valString;
+/**
+ * 
+ */
+difficulty.forEach(level => level.addEventListener('click',(event) => {
+    //if level has been picked already, undo previous color 
+    if(levelSelected){
+        document.getElementById(`${levelSelected}`).classList.remove('buttonSelectBackground');
+        
     }
-  };
-};
+    //Otherwise set background and read in difficulty level
+    event.target.classList.add('buttonSelectBackground');
+    levelSelected = event.target.id;
+
+}));
 
 
-//Read in start button and section for cards to be created in
-const startGame = document.getElementById('start');
-const cardGrid = document.querySelector('.card-grid');
-const elementStart = document.querySelectorAll('.start-items');
-const gameControls = document.querySelectorAll('.game-controls');
-
-
-cardGrid.addEventListener("click", (event) => {
-    // console.log(event)
-    event.target.parentNode.style.transform = "rotateY(180deg)";
-    // event.target.childNode.style.transform = "rotateY(180deg)";
-    
-})
-
-
+/**
+ * 
+ */
 startGame.addEventListener('click',() => {
     //If cards exist then exit event
     let cards = document.querySelectorAll('.flip-card');
     if (cards.length > 0){
         
     }else{
-        //Show game-controls and hide start-items
+        //Hide start-items
         elementStart.forEach(element => {
             element.classList.add("display-none");
         });
+
+        //Unhide game controls
         gameControls.forEach(element => {
             element.classList.remove("display-none");
         });
@@ -142,45 +210,52 @@ startGame.addEventListener('click',() => {
         
         //Run random icon function to get new random array
         randomIcons();
-      
-        //Start timer when game is started
-        startTimer();
 
         //Set initial # of matches at start
-        let numberMatchesLeft = numberOfCards/2;
+        numberMatchesLeft = numberOfCards/2;
         document.getElementById('matches-left').innerText = numberMatchesLeft;
 
         //Start timer when game is started
         startTimer();
 
-
-        //Create a div for every card with the icon inside
-        for (let i = 0; i < numberOfCards; i++){
-            let card = document.createElement('div');
-            let cardInner = document.createElement('div');
-            let cardFront = document.createElement('div');
-            let cardBack = document.createElement('div');
-            let icon = document.createElement('i');
-
-            icon.classList.add('fas');
-            icon.classList.add(`fa-${randomIconArray[i]}`);
-            card.id = randomIconArray[i];
-
-            cardInner.classList.add('flip-card-inner');
-            cardFront.classList.add('flip-card-front');
-            cardBack.classList.add('flip-card-back');
-            card.classList.add('flip-card');
-            
-            cardBack.append(icon);
-            cardInner.append(cardFront);
-            cardInner.append(cardBack);
-            card.append(cardInner);
-            cardGrid.append(card);
-    }
+        //Create new cards
+        createCards();
+        
     }
 
-    
-    
 });
+
+
+/**
+ * 
+ */
+cardGrid.addEventListener("click", (event) => {
+    if(event.target===cardGrid){
+
+    } else {
+        event.target.parentNode.style.transform = "rotateY(180deg)";
+        // event.target.childNode.style.transform = "rotateY(180deg)";
+        if(click1 && click2){
+            click1 = event.target;
+            click2 = null;
+        } else{
+            if (!click1){
+                click1 = event.target;
+            }
+            else if (!click2){
+                click2 = event.target;
+        
+                setTimeout( () => {
+                    if(click1.id !== click2.id){
+                        cardsMismatch(click1, click2);
+                    }else{
+                        cardsMatch(click1, click2);
+                    }
+                }, 1000);
+            }
+        }
+    }
+    
+})
   
 })();
