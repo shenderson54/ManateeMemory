91ce760fec4959329ec94853db45a668deec524d
SHenderson <shenderson5454@gmail.com>
2021-03-16 18:47:05 -0400

    cleaned up typos on html and .js files

--- script.js
+++ script.js
@@ -230,7 +230,7 @@
       || animationRunning
       || event.target.parentNode.className === `card-grid ${levelSelected}`
     ) {
-      //  If click is not exactly on a card or animation is ongoing, dont do anything
+      //  If click is not exactly on a card or animation is ongoing, don't do anything
     } else {
       event.target.parentNode.style.transform = 'rotateY(180deg)';
       if (click1 && click2) {
